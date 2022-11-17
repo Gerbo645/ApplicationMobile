ff93fe85ab514bcfa63ad8f058da921a48cc29b2
Gerbo645 <akarimgerbo11@gmail.com>
2022-11-17 23:00:59 +0100

    mise à jour

--- APP/pizzaApp/pizzaApp/pizzaApp/MainPage.xaml.cs
+++ APP/pizzaApp/pizzaApp/pizzaApp/MainPage.xaml.cs
@@ -15,46 +15,58 @@ namespace pizzaApp
     public partial class MainPage : ContentPage
     {
         List<pizza> pizzas;
+        string PizzaJson;
+        const string URL = "https://drive.google.com/uc?export=download&id=1chXfaxqQCCuEs6qAENsbRrI2v3v98d5c";
 
         public MainPage()
-            
+
         {
             InitializeComponent();
-            maListView.IsVisible= false;
-            waitLayout.IsVisible=true;
-        
-        
-            
+            maListView.IsVisible = false;
+            waitLayout.IsVisible = true;
+
+
 
             pizzas = new List<pizza>();
 
-           
-            const string URL = "https://drive.google.com/uc?export=download&id=1chXfaxqQCCuEs6qAENsbRrI2v3v98d5c";
-            string PizzaJson = "";
-           
 
+            DownloadData();
+
+
+
+        }
+
+        public void DownloadData()
+        {
             using (var webclient = new WebClient())
             {
                 try
                 {
-                    Console.WriteLine("etape 2");
                     // PizzaJson = webclient.DownloadString(URL);
                     //foncion asynchrone 
-                    webclient.DownloadStringCompleted += (object sender, DownloadStringCompletedEventArgs e) => 
+                    webclient.DownloadStringCompleted += (object sender, DownloadStringCompletedEventArgs e) =>
                     {
-                       
-                        PizzaJson =e.Result;
+
+                        PizzaJson = e.Result;
                         pizzas = JsonConvert.DeserializeObject<List<pizza>>(PizzaJson);
 
                         maListView.ItemsSource = pizzas;
-                        maListView.IsVisible = true;
-                        waitLayout.IsVisible = false;
+                        if (maListView.IsRefreshing)
+                        {
+                            maListView.IsRefreshing = false;
+                        }
+                        else
+                        {
+                            maListView.IsVisible = true;
+                            waitLayout.IsVisible = false;
+                        }
+
 
                     };
-                    
+
                     webclient.DownloadStringAsync(new Uri(URL));
                 }
-                catch(Exception ex)
+                catch (Exception ex)
                 {
                     Device.BeginInvokeOnMainThread(() =>
                     {
@@ -64,11 +76,13 @@ namespace pizzaApp
                     return;
                 }
 
-             
+
             }
-            
+        }
 
-
+        private void maListView_Refreshing(object s, EventArgs even)
+        {
+            DownloadData();
         }
     }
 }
